d89f9737819cb5329e38c5aded5a003d256e3e0e
Soomin Kimu <soominkimu@gmail.com>
2017-11-06 19:36:09 -0800

    Chap 4 modified

--- c03/chap04.js
+++ c03/chap04.js
@@ -6,9 +6,9 @@ const DigitalDisplay = function(props) {
 
 const AnalogDisplay = function AnalogDisplay(props) {
 	let date = new Date(props.time)
-	let sDeg = (date.getSeconds()/60)*360 - 90
-	let mDeg = (date.getMinutes()/60)*360 + sDeg/60 - 90	// adjust for the seconds
-	let hDeg = (date.getHours()/12)*360 + mDeg/60 - 90		// adjust for the minutes and seconds
+	let sDeg = (date.getSeconds()/60)*360
+	let mDeg = (date.getMinutes()/60)*360 + sDeg/60		// adjust for the seconds
+	let hDeg = (date.getHours()/12)*360 + mDeg/12		// adjust for the minutes and seconds
 	let dialStyle = {
 		position: 'relative',
 		top: 0,
@@ -26,7 +26,7 @@ const AnalogDisplay = function AnalogDisplay(props) {
 		border: '1px solid red',
 		width: '40%',
 		height: 1,
-		transform: 'rotate(' + sDeg.toString() + 'deg)',
+		transform: 'rotate(' + (sDeg - 90).toString() + 'deg)',
 		transformOrigin: '0% 0%',
 		backgroundColor: 'red'
 	}
@@ -37,7 +37,7 @@ const AnalogDisplay = function AnalogDisplay(props) {
 		border: '1px solid grey',
 		width: '40%',
 		height: 3,
-		transform: 'rotate(' + mDeg.toString() + 'deg)',
+		transform: 'rotate(' + (mDeg - 90).toString() + 'deg)',
 		transformOrigin: '0% 0%',
 		backgroundColor: 'grey'
 	}
@@ -48,7 +48,7 @@ const AnalogDisplay = function AnalogDisplay(props) {
 		border: '1px solid grey',
 		width: '20%',
 		height: 7,
-		transform: 'rotate(' + hDeg.toString() + 'deg)',
+		transform: 'rotate(' + (hDeg - 90).toString() + 'deg)',
 		transformOrigin: '0% 0%',
 		backgroundColor: 'grey'
 	}
