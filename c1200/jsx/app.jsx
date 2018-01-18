fbe5b1b30fb63852d8fed38b5a2995dca82c08dc
Soomin Kimu <soominkimu@gmail.com>
2018-01-17 23:14:43 -0800

    chap13 router added

--- c1200/jsx/app.jsx
+++ c1200/jsx/app.jsx
@@ -3,8 +3,23 @@ require('../css/main.css')
 const React = require('react')
 const ReactDOM = require('react-dom')
 const Content03 = require('./content03.jsx')
+const Router = require('./router.jsx')
+
+const mapping = {
+	'#profile': <div>Profile (<a href="#">home</a>)</div>,
+	'#accounts': <div>Accounts (<a href="#">home</a>)</div>,
+	'*': <div>Dashboard<br/>
+		<a href="#profile">Profile</a>
+		<br/>
+		<a href="#accounts">Accounts</a>
+	</div>
+}
 
 ReactDOM.render(
-	<Content03 />,
+	<div>
+		<Router mapping = {mapping}/>
+		<br />
+		<Content03 />
+	</div>,
 	document.getElementById('chap12')
 )
